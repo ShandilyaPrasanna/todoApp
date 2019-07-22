cbc3a4a17556b051d31ff8e312a26dd0b89ed83a
Prasanna Shandilya <prasanna.shandilya@walmartlabs.com>
2019-07-22 13:33:37 +0530

    Added JSON for mantaing todo data

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,14 +1,25 @@
+const todos=require("./todos.json");
+const fs = require('fs');
+
 export default [{
     method:"GET",
     path:"/",
     handler:function(){
+        let rawdata = fs.readFileSync('src/server/todos.json');
+        console.log("aaaa",JSON.parse(rawdata));
         return "THIS IS HOMEPAGE"
     }
 },{
     method:"POST",
     path:"/addTODO",
     handler:function(request,h){
-        const payload=request.payload;
-        return payload.todo;
+        const {id,text,status}=request.payload;
+        let data = JSON.parse(fs.readFileSync('src/server/todos.json'));
+        if(!data.ids.includes(id)){
+            data.ids.push(id);
+            data.todos[id]={text,status};
+        }
+         fs.writeFileSync('src/server/todos.json', JSON.stringify(data));
+        return "SAVED SUCCESSFULLY";
     }
 }]
